32ee32c35bed24a0b13c42b6777365073ca2ad07
Wiktor Zając <wiktor.zajac888@gmail.com>
2021-02-20 13:29:21 +0100

    remove key from stripe

--- public/scripts/stripe.js
+++ public/scripts/stripe.js
@@ -1,5 +1,5 @@
-var  stripe   = Stripe("pk_test_51GswerA4skDlzpwzJKRKeORlRb2JMjp2apeKa7NUdkUs6IlLJ9BrquOfvIlUC572WMYPLHX54Vu2MuMmfNzOE7Vj00IaxjxCHC"),
-     orderBtn = document.getElementById("order-btn");
+const  stripe   = Stripe("stripe_key_goes_here"),
+       orderBtn = document.getElementById("order-btn");
 
 orderBtn.addEventListener("click", () => {
 
